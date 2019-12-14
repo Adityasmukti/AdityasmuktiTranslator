12e21ff62ca0d48b0b529a3a729e8965b301316b
adityasmukti <adityasmukti@outlook.com>
2019-12-14 17:02:56 +0700

    init

---
+++ Translator/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Translator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBoxBase_OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            string richText = new TextRange(TbSource.Document.ContentStart, TbSource.Document.ContentEnd).Text;
+            TbTarget.Document.Blocks.Clear();
+            TbTarget.Document.Blocks.Add(new Paragraph(new Run(richText)));
+        }
+    }
+}
